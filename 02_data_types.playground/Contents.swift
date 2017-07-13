7647a6894f636fefe063c3fc97f82b5601b58a6e
Macbook <Macbook@naMooSeong.local>
2017-07-13 22:41:22 +0900

    Add data types

---
+++ 02_data_types.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//str = 3
+//var & let
+
+var number = 1
+
+var str2:String = "hello"
+var str3:Int = 1
+
+
+//convert string to integer
+10
+"10"
+
+var numStr = "10"
+
+Int(numStr)
+
+//convert integer to string
+var num3 = 5
+var str4 = "hello"
+"\(num3)"
+"\(str4)! \(num3)"
+
+String(num3)
+
+//integer
+3 / 2
+
+//double / float
+3.0 / 2.0
+
+//double Integer
+Int(3.0 / 2.0)
